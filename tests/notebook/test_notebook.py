d06e22534a53e9fffb3819ae7d3d4882293a10a3
Brian <whax@live.fr>
2022-07-30 09:14:11 +0200

    initialization

---
+++ tests/notebook/test_notebook.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+import pytest
+
+import matplotlib.pyplot as plt
+
+from internal.tools.common_path import NOTEBOOK_PATH
+
+
+@pytest.mark.tuto
+def test_tuto_are_without_output(dir_path=NOTEBOOK_PATH) -> None:
+    # Given
+    from boar.linting import lint_notebook
+
+    # When
+    lint_kwargs = dict(inline=False, verbose=False, recursion_level=-1000)
+    bad_lint_paths = lint_notebook(dir_path, **lint_kwargs)
+
+    # Then
+    if len(bad_lint_paths) != 0:
+        bad_lint_files = [Path(bad_lint_path).name for bad_lint_path in bad_lint_paths]
+        msg = f"Clear output in: {bad_lint_files}"
+        raise Exception(msg)
+
+
+@pytest.mark.tuto
+@pytest.mark.parametrize(
+    "notebook_name",
+    [
+        "k_means",
+    ],
+)
+def test_tuto_runs_without_error(
+    notebook_name: str,
+    dir_path=NOTEBOOK_PATH,
+) -> None:
+    # Given
+    from boar.testing import assert_notebook
+
+    notebook_path = Path(dir_path, notebook_name)
+
+    # When / Then
+    assert notebook_path.exists()
+    assert_notebook(notebook_path, verbose=True)
+
+    # Finally
+    plt.close("all")
