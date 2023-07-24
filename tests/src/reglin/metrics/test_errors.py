18a3bd8678909df0259f0532f212ac99a2a2555c
Kevin Kpakpo <kevin.kpakpo@descartesunderwriting.com>
2023-07-24 13:22:36 +0200

    fix: unit test

--- tests/src/reglin/metrics/test_errors.py
+++ tests/src/reglin/metrics/test_errors.py
@@ -1,16 +1,19 @@
 import numpy as np
 import pandas as pd
-from reglin.metrics.errors import calculer_erreur
+import pytest
 
+
+@pytest.mark.ut
 def test_calculer_erreur_returns_correct_values():
     # Given
-    
+    from reglin.metrics.errors import calculer_erreur
 
     df = pd.DataFrame({"Y": [2,10,5],"X": [0.5,-62,2]})
     beta = [2,6]
     predictor_col = "X"
     target_col = "Y"
     expected_quadratic_error = 74.0135123
+
     # When
     computed_quadratic_error = calculer_erreur(beta, df, predictor_col, target_col)
 
